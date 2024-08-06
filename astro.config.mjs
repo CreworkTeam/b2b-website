a496ef2eaf1f793ce6e7c69aac82fa3f28de010c
Satyajit Chaudhary <thisissatyajit05@gmail.com>
2024-08-06 21:00:20 +0530

    fix: change adapter to vercel from node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,9 +10,7 @@ export default defineConfig({
     actions: true,
   },
   integrations: [tailwind(), react()],
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   vite: {
     build: {
       rollupOptions: {
