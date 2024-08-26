301e9e4e25b6739c4d701d2384d7b08233d44a5c
Sai Tarun <saitarunsst@gmail.com>
2024-08-27 01:11:36 +0530

    chore: Update portfolio section with new images and marquee animation

---
+++ constants.ts
@@ -0,0 +1,27 @@
+const CASE_STUDIES = [
+  {
+    title: 'Instant Business Valuation Web Application',
+    description: 'How we built a quick, data-driven business valuations tool for FundsFinders.',
+    src: '/portfolio/fundsfinder.png',
+    alt: 'FundsFinder',
+    link: '/case-studies/instant-business-valuation-web-application',
+  },
+  {
+    title: 'Custom Learning Management System',
+    description:
+      'How we empowered Crework to increase engagement by deliver tailored learning experiences to aspiring Product Managers',
+    src: '/portfolio/dashboard-2.png',
+    alt: 'Custom Learning Management System',
+    link: '/case-studies/custom-learning-management-system',
+  },
+  {
+    title: 'Global Service Provider Website Redesign',
+    description:
+      'How we helped Travay Group with a complete redesign of their website to improve user experience and navigation.',
+    src: '/portfolio/travaygroup.png',
+    alt: 'Travay Group',
+    link: '/case-studies/global-service-provider-website-redesign',
+  },
+];
+
+export { CASE_STUDIES };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenColorPalette');
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
@@ -12,12 +14,34 @@ export default {
           '0%': { transform: 'translateX(100%)' },
           '100%': { transform: 'translateX(0%)' },
         },
+        scaledown: {
+          '0%': { transform: 'scale(1)' },
+          '100%': { transform: 'scale(0.9)' },
+        },
+        'border-beam': {
+          '100%': {
+            'offset-distance': '100%',
+          },
+        },
       },
       animation: {
         marquee: 'marquee 50s linear infinite',
         marquee2: 'marquee2 50s linear infinite',
+        scaledown: 'scaledown 0.5s ease-in-out forwards',
+        'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme('colors'));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ':root': newVars,
+  });
+}
