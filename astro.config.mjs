db8c53fcf283494df1fb2d6e0192a0fa0a45356b
Sai Tarun <saitarunsst@gmail.com>
2024-09-20 22:10:49 +0530

    Refactor external dependencies in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
   vite: {
     build: {
       rollupOptions: {
-        external: ['react', 'react-dom'],
+        external: ['react-remove-scroll'],
       },
     },
   },
