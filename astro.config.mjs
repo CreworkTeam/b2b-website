db2e86d98ab4dfc0d2992975385a473bb713e9a8
Sai Tarun <saitarunsst@gmail.com>
2024-09-22 19:06:40 +0530

    Refactor file paths and imports in astro.config.mjs and blog-post-layout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
-import path from 'path';
 import sectionize from '@hbsnow/rehype-sectionize';
 
 // https://astro.build/config
@@ -10,10 +9,11 @@ export default defineConfig({
   output: 'server',
   integrations: [tailwind(), react()],
   adapter: vercel(),
+  site: 'https://www.creworklabs.com',
   vite: {
     build: {
       rollupOptions: {
-        external: ['react-remove-scroll'],
+        external: [],
       },
     },
   },
